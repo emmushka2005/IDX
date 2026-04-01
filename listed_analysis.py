9b4c58616168b79c6a5d4c998c9d701d4958957a
emmushka2005 <emmushka2005@gmail.com>
2026-04-01 16:54:09 -0700

    created scripts to combine all csv's for each respective category, 'listing' or 'sold'

--- listed_analysis.py
+++ listed_analysis.py
@@ -1,1 +1,20 @@
 import pandas as pd
+import os
+
+dfs = []
+
+for folder in ["", "raw"]:
+    for file in os.listdir(folder if folder else "."):
+        
+        if file.endswith(".csv") and "listing" in file.lower():
+            
+            path = os.path.join(folder, file) if folder else file
+            df = pd.read_csv(path)
+            dfs.append(df)
+
+combined = pd.concat(dfs)
+
+combined.to_csv("listed.csv", index=False)
+
+print('Listed files successfuly combined.')
+
